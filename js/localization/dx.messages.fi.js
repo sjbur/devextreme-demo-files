b45a940af84ea14d3289d76c8467554c022bf96b
Sergio Bur <sjbur.dev@gmail.com>
2026-03-03 11:50:00 +0100

    aue

--- js/localization/dx.messages.fi.js
+++ js/localization/dx.messages.fi.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.messages.fi.js)
 * Version: 26.1.0
-* Build date: Thu Feb 26 2026
+* Build date: Tue Mar 03 2026
 *
 * Copyright (c) 2012 - 2026 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
